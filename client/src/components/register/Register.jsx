9c14939f5b832b9b3393c0738b6e355d4e7e7ce7
neneaxn <acodesjs@outlook.com>
2025-10-31 15:41:51 +0200

    implemented initial register page

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,7 +1,46 @@
+import { Link } from "react-router-dom";
+import Login from "../login/Login";
+import styles from './Register.module.css';
+
 export default function Register() {
     return(
         <>
-            <h1>This is register page</h1>
+            <section className={styles.register}>
+                <form id="add">
+                    <div className={styles.container}>
+            
+                        <h1>Register</h1>
+                
+                        <label htmlFor="email">Email:</label>
+                        <input 
+                            type="email" 
+                            id="email" 
+                            name="email" 
+                            placeholder="john@outlook.com" 
+                        />
+                
+                        <label htmlFor="register-password">Password:</label>
+                        <input 
+                            type="password" 
+                            name="register-password" 
+                            id="register-password" 
+                        />
+
+                        <label htmlFor="confirm-password">Confirm Password:</label>
+                        <input 
+                            type="password" 
+                            name="confirm-password" 
+                            id="confirm-password" 
+                        />
+
+                        <input className="btnSubmit" type="submit" value="Register"/>
+
+                        {/* <div>
+                            <p>If you already have an account click <Link to={<Login/>}>here</Link></p>
+                        </div> */}
+                    </div>
+                </form>
+            </section>
         </>
     );
 }
