20642856e234de3c8325b487a2c3cbe0d44a8881
neneaxn <acodesjs@outlook.com>
2025-11-04 14:04:09 +0200

    Authentication service added
    login, register, logout

---
+++ client/src/services/authService.js
@@ -0,0 +1,20 @@
+import * as request from '../lib/dataFetcher'
+
+
+const baseUrl = 'http://localhost:3030/users';
+
+export const login = async (username, password) => {
+     const result = await request.post(`${baseUrl}/login`, {
+            email,
+            password,
+        })
+    
+     return result;
+};
+
+export const register = (email, password) => request.post(`${baseUrl}/register`, {
+    email,
+    password,
+});
+
+export const logout = () => request.get(`${baseUrl}/logout`);
