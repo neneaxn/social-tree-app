ceebfd69611c8ebaa695692583288b8e6de2a559
neneaxn <acodesjs@outlook.com>
2025-11-04 16:33:14 +0200

    Logout implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
-import * as authService from '../../client/src/services/authService'
+import * as authService from '../../client/src/services/authService';
 
 import Home from './components/home/Home';
 import Header from './components/header/Header';
@@ -10,31 +10,43 @@ import Register from './components/register/Register'
 import Create from './components/event/create/Create';
 import Events from './components/event/all-events/Events';
 import EventDetails from './components/event/details/Detials';
+import Logout from './components/logout/Logout';
 import AuthContext from './contexts/authContext';
 import Path from './lib/paths';
 
 function App() {
     const navigate = useNavigate();
-    const [auth, setAuth] = useState({});
+    const [auth, setAuth] = useState(() => {
+        localStorage.removeItem('accessToken');
+        return {};
+    });
 
     const loginSubmitHandler = async (values) => {
       const result = await authService.login(values.email, values.password)
       setAuth(result);
+      localStorage.setItem('accessToken', result.accessToken);
       navigate(Path.Home);
     };
 
     const registerSubmitHandler = async (values) => {
       const result = await authService.register(values.email, values.password);
       setAuth(result);
+      localStorage.setItem('accessToken', result.accessToken);
       navigate(Path.Home);
+    };
+
+    const logoutHandler = () => {
+        setAuth({});
+        localStorage.removeItem('accessToken');
     }
 
     const values = {
       loginSubmitHandler,
       registerSubmitHandler,
+      logoutHandler,
       email: auth.email,
-      isAuthenticated: !!auth.email,
-    }
+      isAuthenticated: !!auth.accessToken,
+    };
 
   return (
     <AuthContext.Provider value={values}>
@@ -47,6 +59,7 @@ function App() {
             <Route path={Path.Register} element={<Register/>}/>
             <Route path={Path.CreateEvent} element={<Create/>}/>
             <Route path={Path.EventDetails} element={<EventDetails/>}/>
+            <Route path={Path.Logout} element={<Logout/>}/>
         </Routes> 
     </div>
     </AuthContext.Provider>
---
+++ client/src/components/logout/Logout.jsx
@@ -0,0 +1,21 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import * as authService from '../../services/authService'
+import Path from "../../lib/paths";
+import AuthContext from "../../contexts/authContext";
+
+export default function Logout() {
+    const navigate = useNavigate();
+    const { logoutHandler } = useContext(AuthContext)
+
+    useEffect(() => {
+        authService.logout()
+            .then(() => {
+                logoutHandler();
+                navigate(Path.Home)
+                })
+            .catch(() => navigate(Path.Home))
+    }, []);
+
+    return null;
+}
