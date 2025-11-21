4776a42b61dee8e541e8fbeb363051eee425364d
neneaxn <acodesjs@outlook.com>
2025-11-21 20:32:57 +0200

    Partial loading added to Home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -4,15 +4,21 @@ import styles from './Home.module.css'
 import HomeEvents from './home-events/HomeEvent';
 import Values from './values/Values';
 import UserGuide from './user-guide/UserGuide';
+import Loading from '../loading/Loading';
 
 
 export default function Home() {
     const [events, setEvents] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        setIsLoading(true);
+
         eventService.getAll()
-            .then(result => setEvents(result));
-    }, []);    
+            .then(result => setEvents(result))
+            .catch(err => console.log(`${err.message}: events`))
+            .finally(() => setIsLoading(false))
+    }, []);
 
     return(
         <section className={styles.homePage}>
@@ -28,15 +34,17 @@ export default function Home() {
             <div className={styles.latestEvents}>
                 <h2 className={styles.headingTwo}>Our events</h2>
 
-                {events.length == 0 ? 
-                    (<p className={styles.noEvents}>No upcoming events!</p>) 
-                : 
-                <div className={events.length > 3 ? styles.scrollingSlider : styles.staticRow}> 
-                    <div className={styles.sliderContent}> 
-                        {events.map(event => <HomeEvents key={event._id} {...event}/>)}
+                {isLoading ? (
+                    <Loading />
+                ) : events.length === 0 ? (
+                    <p className={styles.noEvents}>No upcoming events!</p>
+                ) : (
+                    <div className={events.length > 3 ? styles.scrollingSlider : styles.staticRow}> 
+                        <div className={styles.sliderContent}> 
+                            {events.map(event => <HomeEvents key={event._id} {...event}/>)}
+                        </div>
                     </div>
-                </div>
-                }               
+                )}
             </div>
 
             <UserGuide/>
