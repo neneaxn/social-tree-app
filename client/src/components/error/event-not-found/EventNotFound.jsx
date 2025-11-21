ea52d9fc2aba7cc5312b75190d3c434d2e4b6c33
neneaxn <acodesjs@outlook.com>
2025-11-21 18:38:26 +0200

    Error pages css improvements

--- client/src/components/error/event-not-found/EventNotFound.jsx
+++ client/src/components/error/event-not-found/EventNotFound.jsx
@@ -1,9 +1,13 @@
+import Path from '../../../lib/paths'
+import {Link} from 'react-router-dom'
 import styles from '../event-not-found/EventNotFound.module.css'
 
 export default function EventNotFound() {
     return (
         <section className={styles.container}>
-            <p className={styles.EventNotFound}>Event Not Found!</p>
+            <h1 className={styles.headingOne}>Event Not Found!</h1>
+            <p className={styles.info}>Please go back to 
+                <Link to={Path.AllEvents} className={styles.eventBtn}> all events. </Link></p>  
         </section>
         
     )
